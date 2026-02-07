bbd3626fdcfdc0ae3258a0b0b5533a846a33af38
A7medMg <asilme023@gmail.com>
2026-02-07 16:42:19 +0200

    change font family and scaffold background color.

--- lib/tasks_app.dart
+++ lib/tasks_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:todo/core/routing/app_routers.dart';
 import 'package:todo/core/routing/routers_name.dart';
+import 'package:todo/core/theming/colors_manager.dart';
 
 class TasksApp extends StatelessWidget {
   const TasksApp({super.key});
@@ -14,6 +15,8 @@ class TasksApp extends StatelessWidget {
       builder: (context, child) {
             return MaterialApp(
           theme: ThemeData(
+            scaffoldBackgroundColor: ColorsManager.white,
+            fontFamily: "DMSans"
           ),
           debugShowCheckedModeBanner: false,
           initialRoute: RoutersName.onboarding,
